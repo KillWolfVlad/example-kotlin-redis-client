61f9bfd8eb7c5fe318721565d91ba4753d37ea2e
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2025-02-19 17:47:52 +0500

    style: fix tests formatting

--- src/test/kotlin/ru/killwolfvlad/redis/RedisClientTest.kt
+++ src/test/kotlin/ru/killwolfvlad/redis/RedisClientTest.kt
@@ -7,66 +7,65 @@ import ru.killwolfvlad.redis.commands.get
 import ru.killwolfvlad.redis.commands.ping
 import ru.killwolfvlad.redis.commands.set
 
-class RedisClientTest :
-    DescribeSpec({
-        val redisConnectionString = "redis://localhost:6379"
+class RedisClientTest : DescribeSpec({
+    val redisConnectionString = "redis://localhost:6379"
 
-        val jedis = JedisPooled(redisConnectionString)
-        val redisClient = RedisClient(redisConnectionString)
+    val jedis = JedisPooled(redisConnectionString)
+    val redisClient = RedisClient(redisConnectionString)
 
-        beforeSpec {
-            redisClient.init()
-        }
+    beforeSpec {
+        redisClient.init()
+    }
 
-        beforeEach {
-            jedis.flushDB()
-        }
+    beforeEach {
+        jedis.flushDB()
+    }
 
-        afterSpec {
-            redisClient.close()
-        }
+    afterSpec {
+        redisClient.close()
+    }
 
-        it("ping without arguments") {
-            redisClient.ping() shouldBe "PONG"
-        }
+    it("ping without arguments") {
+        redisClient.ping() shouldBe "PONG"
+    }
 
-        it("ping with arguments") {
-            redisClient.ping("Привет! \uD83D\uDC4B") shouldBe "Привет! \uD83D\uDC4B"
-        }
+    it("ping with arguments") {
+        redisClient.ping("Привет! \uD83D\uDC4B") shouldBe "Привет! \uD83D\uDC4B"
+    }
 
-        it("set when key doesn't exist") {
-            redisClient.set("key1", "value1") shouldBe "OK"
+    it("set when key doesn't exist") {
+        redisClient.set("key1", "value1") shouldBe "OK"
 
-            jedis.get("key1") shouldBe "value1"
-        }
+        jedis.get("key1") shouldBe "value1"
+    }
 
-        it("set empty value") {
-            redisClient.set("key1", "") shouldBe "OK"
+    it("set empty value") {
+        redisClient.set("key1", "") shouldBe "OK"
 
-            jedis.get("key1") shouldBe ""
-        }
+        jedis.get("key1") shouldBe ""
+    }
 
-        it("set when key exists") {
-            jedis.set("key1", "value2")
+    it("set when key exists") {
+        jedis.set("key1", "value2")
 
-            redisClient.set("key1", "value1") shouldBe "OK"
+        redisClient.set("key1", "value1") shouldBe "OK"
 
-            jedis.get("key1") shouldBe "value1"
-        }
+        jedis.get("key1") shouldBe "value1"
+    }
 
-        it("get when key doesn't exists") {
-            redisClient.get("key1") shouldBe null
-        }
+    it("get when key doesn't exists") {
+        redisClient.get("key1") shouldBe null
+    }
 
-        it("get empty value") {
-            jedis.set("key1", "")
+    it("get empty value") {
+        jedis.set("key1", "")
 
-            redisClient.get("key1") shouldBe ""
-        }
+        redisClient.get("key1") shouldBe ""
+    }
 
-        it("get when key exists") {
-            jedis.set("key1", "value1")
+    it("get when key exists") {
+        jedis.set("key1", "value1")
 
-            redisClient.get("key1") shouldBe "value1"
-        }
-    })
+        redisClient.get("key1") shouldBe "value1"
+    }
+})
